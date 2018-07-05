130758f8872d16994cbf9b62b58295135b7cbab4
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2018-07-05 09:34:02 -0300

    initial commit

---
+++ src/main/java/br/com/casadocodigo/loja/daos/UsuarioDAO.java
@@ -0,0 +1,46 @@
+package br.com.casadocodigo.loja.daos;
+
+import br.com.casadocodigo.loja.models.Usuario;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsuarioDAO implements UserDetailsService {
+
+  @PersistenceContext
+  private EntityManager manager;
+
+  public Usuario loadUserByUsername(String email) {
+    List<Usuario> usuarios = manager
+        .createQuery("select u from Usuario u where u.email = :email", Usuario.class)
+        .setParameter("email", email).getResultList();
+
+    if (usuarios.isEmpty()) {
+      throw new UsernameNotFoundException("Usuário " + email + " não foi encontrado");
+    }
+
+    return usuarios.get(0);
+  }
+
+  public void gravar(Usuario usuario) {
+    manager.persist(usuario);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
